bbcc8d6aee06af2a0e8e86b809e38794f692aafb
goranaviani <goranaviani@gmail.com>
2020-05-08 16:00:05 +0200

    iterrows()

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pandas
 def main():
     df = pandas.read_csv('example.csv')
     for index, row in df.iterrows():
-        print("index {}, row: {}" .format(index, row))
+        print("index {}".format(index))
+        print("-------------")
+        print("row: {}".format(row))
 
 
 if __name__ == "__main__":
