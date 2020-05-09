38ff6f4b040a1630b3f577f60fd22828885f2a9c
goranaviani <goranaviani@gmail.com>
2020-05-09 20:18:55 +0200

    itertuples()

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def looping_with_iterrows(file_data):
         print("index {}".format(index))
         print("-------------")
         print("row: {}".format(row))
+def looping_with_itertuples(file_data):
+    for row in file_data.itertuples():
+        print("-------------")
+        print("row: {}".format(row))
 
 def main():
     df = pandas.read_csv('example.csv')
-    looping_with_iterrows(df)
+    looping_with_itertuples(df)
 
 if __name__ == "__main__":
     main()
