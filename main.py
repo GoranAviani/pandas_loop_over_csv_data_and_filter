628c6e5d18555fccbdc24e14398989bc1d209896
goranaviani <goranaviani@gmail.com>
2020-05-10 15:58:01 +0200

    Added function to read csv files with different separators

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas
+
+def reading_file_with_sep(file_name):
+    """
+    If the separator between each
+     field of your data is not a comma, use the sep argument
+    :return: 
+    """
+    df = pandas.read_csv(file_name, header=0, sep=",")
+    return df
+
 def looping_with_iterrows(file_data):
     for index, row in file_data.iterrows():
         print("-------------")
@@ -11,7 +21,9 @@ def looping_with_itertuples(file_data):
         print("row: {}".format(row))
 
 def main():
-    df = pandas.read_csv('example_2.csv', header=0)
+    #file_name = "example_with_comma_dot_separator.csv"
+    file_name ="example_2.csv"
+    df = reading_file_with_sep(file_name)
     looping_with_iterrows(df)
 
 if __name__ == "__main__":
