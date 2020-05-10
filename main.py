784af77caee48b8ef4778ebff98f9490d5a1c88b
goranaviani <goranaviani@gmail.com>
2020-05-10 15:33:11 +0200

    test

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def looping_with_iterrows(file_data):
     for index, row in file_data.iterrows():
 #        print("index {}".format(index))
         print("-------------")
-        print("row: {}".format(row))
+        #print("row: {}".format(row))
+        print(row["field2"])
 def looping_with_itertuples(file_data):
     for row in file_data.itertuples():
         print("-------------")
